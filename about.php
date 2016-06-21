5b3f95a1ab2d2d818dcc8bd7132a1429782c667a
kusayp <josephkusaytetteh@gmail.com>
2016-06-21 15:30:36 +0000

    home
    home

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>CBBC | About</title>
+    <?php include 'components/css.php'; ?>
+    <link rel="shortcut icon" href="favicon.ico"/>
+
+  </head>
+  <!-- #########   BEGIN BODY   ################### -->
+  <body class="page-quick-sidebar-over-content">
+    <!-- BEGIN MAIN LAYOUT -->
+    <?php include 'components/header.php'; ?>
+    <!-- PAGE CONTENT BEGIN -->
+      <div class="container-fluid">
+      	<div class="page-content page-content-popup">
+      		<!-- BEGIN PAGE CONTENT FIXED -->
+  			<?php include 'components/fixed_nav.php'; ?>
+
+  			<!-- BEGIN SIDEBAR -->
+        <?php include 'components/sidemenu.php'; ?>
+  			<!-- END SIDEBAR -->
+
+
+  </body>
+</html>
